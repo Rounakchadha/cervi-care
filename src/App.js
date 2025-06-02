ff7aa0c0b8b4b84de814b04b3afd5c53a4ddbfcb
rounak chadha <131700037+Rounakchadha@users.noreply.github.com>
2025-06-02 13:49:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {
   useLocation,
 } from 'react-router-dom';
 
-import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import ForgotPassword from './pages/ForgotPassword';
